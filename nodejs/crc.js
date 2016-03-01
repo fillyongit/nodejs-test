adb1f332fdadf6fb63224b3068f498c2942ec823
alessio <filippini.alessio@gmail.com>
2016-03-01 11:38:35 +0100

    Crc...

--- nodejs/crc.js
+++ nodejs/crc.js
@@ -4,13 +4,61 @@ var int32View = new Int32Array(buffer);
 for (var i = 0; i < int32View.length; i++) {
 	  int32View[i] = i * 2;
 }
+for (var i = 0; i < int32View.length; i++) {
+	console.log("Entry " + i + ": " + int32View[i]);
+}
+
+console.log(" ------------------------- ");
+
+var float64Array = new Float64Array(buffer);
+for (var i = 0; i < float64Array.length; i++) {
+	console.log("Entry " + i + ": " + float64Array[i]);
+}
+
+console.log(" ------------------------- ");
 
 var int16View = new Int16Array(buffer);
 for (var i = 0; i < int16View.length; i++) {
 	  console.log("Entry " + i + ": " + int16View[i]);
 }
+
 console.log(" ------------------------- ");
+
 var int8View = new Int8Array(buffer);
 for (var i = 0; i < int8View.length; i++) {
 	  console.log("Entry " + i + ": " + int8View[i]);
+}
+
+console.log(" ------------------------- ");
+
+// Un oggetto buffer in Node è sempre una specializzazione di UInt8Array,
+// i sui elementi sono sempre chunk di 8 bit.
+// In questo caso uso uno dei suoi tanti costruttori: 
+// "Allocates a new Buffer using an array of octets".
+var hdr1 = new Buffer([0x62,0x75,0x66,0x66,0x65]);
+var hdr2 = new Buffer([0x04,0x12,0x01,0x34,0x68]);
+
+for(var b of hdr1) {
+	console.log("Entry " + b);
+}
+
+console.log(" ------------------------- ");
+
+var buffer = new ArrayBuffer(8);
+var int16View = new Int16Array(buffer);
+// Essendo il buffer di 8 byte e la DataView è Int16Array
+// devo riempire 4 elementi da 16 bit ciascuno (8/2=4).
+int16View[0] = 0b0000000000000001;
+int16View[1] = 0b0111111111111111;
+int16View[2] = 0b0000000000000011;
+int16View[3] = 0b1111111111111011; // -5 in complemento a 2, perchè Int16Array (come le altre view) memorizza gli interi in complemento a 2.
+var sum = 0;
+for (var i = 0; i < int16View.length; i++) {
+	  console.log("Entry " + int16View[i]);
+	  sum += int16View[i];
+	  console.log("Sum " + sum);
+	  console.log(sum & 0xFFFFF000);
+	  if (sum & 0xFFFFF000) {
+		  console.log("Overflow!");
+	  }
 }
